d9b2e5722f882da64b72e338cbb5233475c5fe7e
chaitanya gollapudi <glchaitanya94@gmail.com>
2025-06-29 18:51:43 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Photos } from './components/photos/photos';
+import { Events } from './components/events/events';
+import { Videos } from './components/videos/videos';
+import { Contact } from './components/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'home', component: Home},
+    {path: 'photos', component: Photos},
+    {path: 'events', component: Events},
+    {path: 'videos', component: Videos},
+    {path: 'contact', component: Contact},
+];
