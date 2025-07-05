37d19e2e7bc7aaa3679b8dba7f410da773b14df1
chaitanya gollapudi <glchaitanya94@gmail.com>
2025-07-05 20:03:42 +0530

    reset contact form

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -30,6 +30,11 @@ export class Contact implements OnInit {
     console.log(data)
     this.http.post('https://ganamruthamserver-b2rc.vercel.app/sendemail', data).subscribe(response => {
       console.log(response)
+      this.contactForm = new FormGroup({
+        name: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(2)]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        message: new FormControl('', [Validators.required, Validators.maxLength(30), Validators.minLength(2)])
+      })
     })
   }
 
